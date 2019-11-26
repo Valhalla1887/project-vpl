9c50d760037285ef718d9cb0d9ef55bc54ecb5c3
J. Remstedt <49120673+Valhalla1887@users.noreply.github.com>
2019-11-26 19:56:54 +0100

    Folder

---
+++ src/components/Documents.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+     <q-list bordered separator class="bg-white">
+      <q-item clickable v-ripple class="bg-green">
+        <q-item-section>Documents</q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <div class="row q-col-gutter-lg">
+         <q-checkbox align="right" left-label="Output" v-model="val"/>
+       </div>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label >
+          <q-uploader
+        url="http://localhost:4444/upload"
+        label="Select Document"
+        color="white"
+        text-color="black"
+        style="max-width: 200px"
+      />
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+        <q-item-section>
+         <q-btn color="grey">Save</q-btn>
+         <q-btn color="red">Delete</q-btn>
+        </q-item-section>
+    </q-list>
+  <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {
+      val: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+}
+</style>
