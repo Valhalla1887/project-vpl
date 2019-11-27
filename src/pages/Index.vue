a511f8371ad9efa50407fc716e782ab49912e3c1
G Schwita <gerrit.schwitalla@yahoo.de>
2019-11-27 11:17:28 +0100

    ---> USE THIS FOR ASSESSMENT
    Added SaveDoc block
    Adjusted block spawn locations

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,4 +1,3 @@
-  
 <template>
   <q-page>
     <Documents  @mousedown.native="moveStart($event, 'Documents', i-1)" @mouseup.native="moveEnd($event, 'Documents', i-1)" @mousemove.native="moveActive($event, 'Documents', i-1)"
@@ -10,13 +9,18 @@
      <OCR  @mousedown.native="moveStart($event, 'OCR', i-1)" @mouseup.native="moveEnd($event,'OCR', i-1)" @mousemove.native="moveActive($event, 'OCR', i-1)"
       class="movable" :ref="'OCR'+(i-1)"
      :style="{'left': positionsOCR[i-1][0] + 'px', 'top': positionsOCR[i-1][1] + 'px'}" v-for="i in positionsOCR.length" :key="'OCR'+i"></OCR>
+     <SaveDoc  @mousedown.native="moveStart($event, 'SaveDoc', i-1)" @mouseup.native="moveEnd($event,'SaveDoc', i-1)" @mousemove.native="moveActive($event, 'SaveDoc', i-1)"
+      class="movable" :ref="'SaveDoc'+(i-1)"
+     :style="{'left': positionsSaveDoc[i-1][0] + 'px', 'top': positionsSaveDoc[i-1][1] + 'px'}" v-for="i in positionsSaveDoc.length" :key="'SaveDoc'+i"></SaveDoc>
      <q-btn @click="addItem('Documents')">Add Documents block</q-btn>
      <q-btn @click="addItem('Saturation')">Add Saturation block</q-btn>
      <q-btn @click="addItem('OCR')">Add OCR block</q-btn>
+     <q-btn @click="addItem('SaveDoc')">Add SaveDoc block</q-btn>
      <hr/>
      <q-btn @click="removeItem('Documents')">Remove Documents Block</q-btn>
      <q-btn @click="removeItem('Saturation')">Remove Saturation Block</q-btn>
      <q-btn @click="removeItem('OCR')">Remove OCR Block</q-btn>
+     <q-btn @click="removeItem('SaveDoc')">Remove SaveDoc Block</q-btn>
      <hr/>
 
   </q-page>
@@ -34,13 +38,15 @@
 import Documents from '../components/Documents.vue'
 import Saturation from '../components/Saturation.vue'
 import OCR from '../components/OCR.vue'
+import SaveDoc from '../components/SaveDoc.vue'
 import { TouchPan } from 'quasar'
 export default {
   name: 'PageIndex',
   components: {
     Documents,
     Saturation,
-    OCR
+    OCR,
+    SaveDoc
   },
   directives: {
     TouchPan
@@ -52,7 +58,8 @@ export default {
       // Any addition to the array will create a new element to the screen
       positionsDocuments: [],
       positionsSaturation: [],
-      positionsOCR: []
+      positionsOCR: [],
+      positionsSaveDoc: []
     }
   },
   methods: {
@@ -67,11 +74,13 @@ export default {
       // Read this:
       // https://vuejs.org/2016/02/06/common-gotchas/
       if (type === 'Documents') {
-        this['positions' + type].push([Math.random() * 1000, 50])
+        this['positions' + type].push([Math.random() * 1000, 100])
       } else if (type === 'Saturation') {
-        this['positions' + type].push([Math.random() * 1000, 250])
+        this['positions' + type].push([Math.random() * 1000, 300])
+      } else if (type === 'OCR') {
+        this['positions' + type].push([Math.random() * 1000, 500])
       } else {
-        this['positions' + type].push([Math.random() * 1000, 450])
+        this['positions' + type].push([Math.random() * 1000, 700])
       }
     },
     moveStart: function (event, type, index) {
